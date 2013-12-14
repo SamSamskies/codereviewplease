19de0f88627cb1683647b5674d28787fb8c8f35f
Elliot Shiu <elliot@sandbochs.com>
2013-12-13 22:47:14 -0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 @user = User.create username: Faker::Internet.user_name
 
 5.times do
-  @request = @user.requests.create description: Faker::Lorem.paragraph, gist_url: Faker::Internet.url
+  @request = @user.requests.create title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, gist_url: Faker::Internet.url
 
   3.times { @request.comments.create body: Faker::Lorem.sentence }
   @tag = @request.tags.create name: Faker::Lorem.word
