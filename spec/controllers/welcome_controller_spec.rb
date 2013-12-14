0338945a786c54370a914e800ca898541b042654
Eric Savvy <eric.chen0121@gmail.com>
2013-12-13 22:07:59 -0800

    added all rspec shoulda tests for class associations

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -7,6 +7,9 @@ end
 describe Request do
   it { should validate_presence_of(:gist_url) }
   it { should validate_presence_of(:description) }
+
+  it { should_have_many(:comments)}
+  it { should_have_and_belong_to_many(:tags)}
 end
 
 describe User do
@@ -14,8 +17,12 @@ describe User do
   # it { should validate_presence_of(:github_token) }
   # it { should validate_presence_of(:avator) }
   # it { should validate_presence_of(:email) }
+  
+  it { should have_many(:requests)}
 end
 
 describe Tag do
   it { should validate_presence_of(:body) }
+
+  it { should_have_and_belong_to_many(:requests)
 end
