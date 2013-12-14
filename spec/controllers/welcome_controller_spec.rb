05d3bc9eea17eeec2034dbb1d75d12ef47a2618b
Eric Savvy <eric.chen0121@gmail.com>
2013-12-13 21:58:13 -0800

    rspec: shoulda matchers for validating presence for all models

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
-describe WelcomeController do
+describe Comment do
+  it { should validate_presence_of(:body) }
+end
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
+describe Request do
+  it { should validate_presence_of(:gist_url) }
+  it { should validate_presence_of(:description) }
+end
 
+describe User do
+  it { should validate_presence_of(:username) }
+  # it { should validate_presence_of(:github_token) }
+  # it { should validate_presence_of(:avator) }
+  # it { should validate_presence_of(:email) }
+end
+
+describe Tag do
+  it { should validate_presence_of(:body) }
 end
