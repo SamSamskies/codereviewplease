2d02f5130fcf4cd619ec5d483703216ed97fc7af
Elliot Shiu <elliot@sandbochs.com>
2013-12-13 23:39:47 -0800

    Embed gist on request show page and update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
-@user = User.create username: Faker::Internet.user_name
+u = User.create username: Faker::Internet.user_name
+r = u.requests.create title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, gist_url: 'https://gist.github.com/sandbochs/99ed6d98783917153f8e'
+3.times { r.comments.create body: Faker::Lorem.sentence }
+r.tags.create name: Faker::Lorem.word
 
-5.times do
-  @request = @user.requests.create title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, gist_url: Faker::Internet.url
+r = u.requests.create title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, gist_url: 'https://gist.github.com/sandbochs/fe8d9607eeffaddb2b9c'
+3.times { r.comments.create body: Faker::Lorem.sentence }
+r.tags.create name: Faker::Lorem.word
 
-  3.times { @request.comments.create body: Faker::Lorem.sentence }
-  @tag = @request.tags.create name: Faker::Lorem.word
-end
+r = u.requests.create title: Faker::Lorem.sentence, description: Faker::Lorem.paragraph, gist_url: 'https://gist.github.com/sandbochs/22cf06436cd72de338e1'
+3.times { r.comments.create body: Faker::Lorem.sentence }
+r.tags.create name: Faker::Lorem.word
