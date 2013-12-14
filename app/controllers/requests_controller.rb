91ae40826d51d3ac7d6fa678949c02ae78c302fc
Elliot Shiu <elliot@sandbochs.com>
2013-12-13 23:16:28 -0800

    Add request create form

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,4 +6,24 @@ class RequestsController < ApplicationController
   def show
     @request = Request.find params[:id]
   end
+
+  def new
+    @request = Request.new
+  end
+
+  def create
+    @request = Request.new request_params
+
+    if @request.save
+      redirect_to request_path(request)
+    else
+      render 'requests/new'
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit :gist_url, :title, :description
+  end
 end
